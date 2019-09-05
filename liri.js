e68c011c128b5a2821863c385642e310b9212b47
dbell <greenadvisor@gmail.com>
2019-09-05 16:48:00 -0400

    initial files

---
+++ liri.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
+
+var arg2 = process.argv[2];
+var arg3 = process.argv[3];
+
+* `concert-this`
+
+"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+
+
+* `spotify-this-song`
+
+* `movie-this`
+
+* `do-what-it-says`
