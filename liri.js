36582896af2bf301e7d50d29e01e3fd77dbcb87e
dbell <greenadvisor@gmail.com>
2019-09-05 17:42:46 -0400

    concert-this working

--- liri.js
+++ liri.js
@@ -1,17 +1,45 @@
 require("dotenv").config();
 var keys = require("./keys.js");
+
+const axios = require('axios');
+var moment = require('moment');
+
+var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 
 var arg2 = process.argv[2];
 var arg3 = process.argv[3];
 
-* `concert-this`
+switch (arg2) {
+    case "concert-this":
+      concert();
+      break;
+    
+    case "spotify-this-song":
+      spotify();
+      break;
+    
+    case "movie-this":
+      movie();
+      break;
+    
+    case "do-what-it-says":
+      doIt();
+      break;
+    }
 
-"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
-
-
-* `spotify-this-song`
-
-* `movie-this`
-
-* `do-what-it-says`
+function concert() {
+    axios.get("shttp://rest.bandsintown.com/artists/" + arg3 + "/events?app_id=codingbootcamp")
+    .then(
+        function(response) {
+            for(i=0; i < response.data.length; i++){
+                console.log(
+                    response.data[i].venue.name + ", " +
+                    response.data[i].venue.city + ", " +
+                    response.data[i].venue.region + ", " +
+                    moment(response.data[i].datetime).format("MM/DD/YYYY")
+                )
+            }
+        }
+    )
+}
